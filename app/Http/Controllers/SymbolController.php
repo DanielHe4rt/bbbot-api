4780c06a799522e376a9e62d99051f80ab40fa18
Daniel He4rt <idanielreiss@gmail.com>
2020-04-14 20:32:55 -0300

    agora vai

--- app/Http/Controllers/SymbolController.php
+++ app/Http/Controllers/SymbolController.php
@@ -6,13 +6,15 @@ namespace App\Http\Controllers;
 
 use App\Entities\Symbol;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class SymbolController extends Controller
 {
     public function getSymbol(Request $request, string $symbol){
+        $symbol = str_replace(['%20',' '],'-',$symbol);
         $model = Symbol::where('name', $symbol)->first();
         if(!$model){
-            $model = Symbol::create(['name' => $symbol]);
+            Symbol::create(['name' => $symbol]);
             return response()->json(['no existe mas agr existe'],404);
         }
         $response = $model->data()->get();
